62fb1db603eaeb9f2d354eb379ebfb7c9ccf577d
worldofchris <chris@worldofchris.com>
2019-12-26 17:04:45 +0000

    Added VIC 20

--- README.md
+++ README.md
@@ -9,7 +9,15 @@ Starting with the Commodore 64:
 
 ![Commodore 64](assets/hello-world-c64.png)
 
+And the VIC 20:
+
+![VIC 20](assets/hello-world-vic20.png)
+
 # Usage
 
-Install depenencies with `install_tools.sh` then make with `make`.
+Install depenencies with `install_tools.sh` then make with:
 
+* `C64` - `make EMU=x64 PLATFORM=C64` then type `RUN` in the emulator.
+* `VIC 20` - `make EMU=xvic PLATFORM=VIC20` then type `SYS 4352` in the emulator.
+
+You'll need to `make clean` when switching platforms.
--- hello.asm
+++ hello.asm
@@ -1,15 +1,20 @@
-.var screen = $0400
+#if PLATFORM_VIC20
+    .var screen = $1e00
+    *=$1100
+#else
+    // Assume it's a C64
+    .var screen = $0400
+    BasicUpstart2(start)
+#endif
 
-:BasicUpstart2(start)
+start:      ldx #$00
+loop:       lda message,x
+            beq exit
+            sta screen,x
+            inx
+            jmp loop
 
-start:		ldx #$00
-loop:		lda message,x
-		beq exit
-		sta screen,x
-		inx
-		jmp loop
+exit:       rts
 
-exit:		rts
-
-message:	.text "hello world!"
-		.byte $00
+message:    .text "hello world!"
+            .byte $00
